f63c482d25114abda22ca3eb08380ce00829f3c5
NikaZhenya <nika.zhenya@gmail.com>
2016-04-17 22:02:22 -0500

    Adición del script en ruby para Tesseract.

---
+++ otros/README.md
@@ -0,0 +1,7 @@
+#Tessractodo
+
+Este pequeño script en ruby ayuda a utilizar Tesseract en todas las imágenes PNG o TIF de la carpeta seleccionada.
+
+##Dependencias
+
+Requiere Tesseract y Ruby.
---
+++ otros/tesseractodo.rb
@@ -0,0 +1,17 @@
+puts "\nEste script ayuda a utilizar Tesseract en múltiples archivos PNG o TIF contenidos en una misma carpeta."
+puts "Requiere tener Tesseract instalado."
+puts "No olvides estar en el directorio donde quieres los archivos de salida."
+puts "\nEscribe el prefijo del lenguaje a detectar"
+lenguaje = gets.chomp
+puts "\nArrastra la carpeta que contiene las imágenes"
+carpeta = gets.chomp.strip
+
+Dir.foreach(carpeta) do |archivo|
+  if File.extname(archivo) == '.png' or File.extname(archivo) == '.tiff'
+      puts "\nEjecutando Tesseract para: #{archivo}"
+      comando = system ("tesseract -l #{lenguaje} #{carpeta + "/" + archivo.gsub(' ', '\ ')} #{archivo.gsub(' ', '\ ').gsub('.png', '').gsub('.tiff', '')} pdf")
+      if comando == false
+          puts "Al parecer tu sistema no tiene instalado Tesseract..."
+      end
+  end
+end
