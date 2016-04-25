12ce5f15d089b95a1a0c0c119665823267e05365
NikaZhenya <nika.zhenya@gmail.com>
2016-04-25 10:55:18 -0500

    Actualización del script de Ruby.

--- otros/tesseractodo.rb
+++ otros/tesseractodo.rb
@@ -13,8 +13,9 @@ end
 Dir.foreach(carpeta.gsub('\ ', ' ').gsub('\'', '')) do |archivo|
   if File.extname(archivo) == '.png' or File.extname(archivo) == '.tiff' or File.extname(archivo) == '.tif'
       puts "\nEjecutando Tesseract para: #{archivo}"
-      comando = system ("tesseract -l #{lenguaje} #{carpeta + "/" + archivo.gsub(' ', '\ ')} #{archivo.gsub(' ', '\ ').gsub('.png', '').gsub('.tiff', '')} pdf")
-      if comando == false
+      comando = system ("tesseract -l #{lenguaje} #{carpeta + "/" + archivo.gsub(' ', '\ ')} #{archivo.gsub(' ', '\ ').gsub('.png', '').gsub('.tiff', '').gsub('.tif', '')}")
+      comando2 = system ("tesseract -l #{lenguaje} #{carpeta + "/" + archivo.gsub(' ', '\ ')} #{archivo.gsub(' ', '\ ').gsub('.png', '').gsub('.tiff', '').gsub('.tif', '')} pdf")
+      if comando == false or comando2 == false
           puts "Al parecer tu sistema no tiene instalado Tesseract..."
       end
   end
